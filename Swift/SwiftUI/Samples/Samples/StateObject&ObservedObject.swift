2ca986bddd862cb2bce80e8e92b16afa48c7d484
yuk1ch1 <sma2ww@gmail.com>
2022-10-16 19:52:12 +0900

    コミットもれ

--- Swift/SwiftUI/Samples/Samples/StateObject&ObservedObject.swift
+++ Swift/SwiftUI/Samples/Samples/StateObject&ObservedObject.swift
@@ -12,17 +12,27 @@
 // ## @StateObject と @ObservedObjectの違い
 // StateObjectCounter: 親Viewのcounterが更新されても値を保持している。Viewに対してインスタンスが1つだけ生成されるため
 // OvservedObjectCounter: Viewのライサイクルに依存しているので、親Viewが再描画される度(親Viewのプロパティが更新される度)に更新され保持するViewが再描画される。実装方法によっては初期化されてしまう
+//
+// ref: https://tokizuoh.dev/posts/td152tqrb7iflicp/
+// ref: https://note.com/taatn0te/n/n1fc0518a8ee6
+//  ref: https://zenn.dev/usk2000/articles/7a05caaa1dd920
+//  ref: https://zenn.dev/yorifuji/articles/swiftui-manage-observableobject
 
 import SwiftUI
 
 struct CounterView: View {
     @State var counter = 0
+    @State var counter2 = 0  // 追加
+    
     var body: some View {
         VStack(alignment: .leading, spacing: 50) {
             HStack {
                 Text("counter: \(counter)")
                 Button("+") {
                     counter += 1
+                }
+                Button("■") { // このcounter2の値をCounterViewで使わない限り、ObservedObjectCounterが再描画されるようなことも特になかった
+                    counter2 += 1  // 追加
                 }
             }
             StateObjectCounter()
