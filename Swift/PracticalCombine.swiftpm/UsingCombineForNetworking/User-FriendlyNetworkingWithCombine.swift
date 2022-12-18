96ba44ed7167857c75a1cc0c00c4de6b0faa3675
yuk1ch1 <sma2ww@gmail.com>
2022-12-17 21:38:05 -0800

    BuildingAComplexChainOfNetworkCallsまで

---
+++ Swift/PracticalCombine.swiftpm/UsingCombineForNetworking/User-FriendlyNetworkingWithCombine.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by s_yamada on 2022/12/17.
+//
+
+import Foundation
+
+/*
+ feature in iOS13 or later, Apple introduced.
+ - Low Data Mode
+ - allowsExpensiveNetworkAccess property
+ */
+
+
